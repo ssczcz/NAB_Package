b9c952544ac145e92a0e4f58182bc7039540318e
ssczcz <35703050+ssczcz@users.noreply.github.com>
2018-02-28 00:06:17 -0500

    Update Score_Player.R

--- ScorePlayer/R/Score_Player.R
+++ ScorePlayer/R/Score_Player.R
@@ -9,7 +9,7 @@
 
 
 Score_Player <- function(x) {
-  load("~/Rpackage/ScorePlayer/data/Seasons_Stats_NBA.RData")
+  load("~/NAB_Package/ScorePlayer/data/Seasons_Stats_NBA.RData")
   dplyr::arrange(filter(d, Year == x), desc(PTS))[1, 2]
 }
 
