572003965e93a48d18f3e8a645dbb6957a2dd88e
ssczcz <35703050+ssczcz@users.noreply.github.com>
2018-02-28 00:25:30 -0500

    Update Oldest_Player.R

--- OldestPlayer/R/Oldest_Player.R
+++ OldestPlayer/R/Oldest_Player.R
@@ -9,7 +9,7 @@
 
 
 Oldest_Player <- function(x) {
-  load("ssczcz/NAB_Package/OldestPlayer/data/Seasons_Stats_NBA.RData")
+  load("~/NAB_Package/OldestPlayer/data/Seasons_Stats_NBA.RData")
   d$Player
 }
 
